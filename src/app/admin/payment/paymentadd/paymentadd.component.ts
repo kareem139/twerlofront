22a7a04f273b9f01dfe83f073cad52a522fe94d2
kareemRabea13 <ksaad@it-blocks.com>
2022-02-24 16:27:44 +0200

    edit theme

--- src/app/admin/payment/paymentadd/paymentadd.component.ts
+++ src/app/admin/payment/paymentadd/paymentadd.component.ts
@@ -21,30 +21,30 @@ export class PaymentaddComponent implements OnInit {
   };
 
   data:any=[
-    {id:112345678912345,to:"Mac",rider:12345678912345,amount:50000,status:1,sender:12345678912345},
-    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:300,status:1,sender:12345678912345},
-    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345},
-    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:1,sender:12345678912345},
-    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:712345678912345,to:"Mac",rider:12345678912345,amount:1000,status:1,sender:12345678912345},
-    {id:112345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:3000,status:1,sender:12345678912345},
-    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345},
-    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:1,sender:12345678912345},
-    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:712345678912345,to:"Mac",rider:12345678912345,amount:900,status:1,sender:12345678912345},
-    {id:112345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:300,status:1,sender:12345678912345},
-    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345},
-    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:1,sender:12345678912345},
-    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:712345678912345,to:"Mac",rider:12345678912345,amount:900,status:1,sender:12345678912345},
-    {id:112345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:300,status:1,sender:12345678912345},
-    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345},
-    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:1,sender:12345678912345},
-    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345},
-    {id:712345678912345,to:"Mac",rider:12345678912345,amount:900,status:1,sender:12345678912345},
+    {id:112345678912345,to:"Mac",rider:12345678912345,amount:50000,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:300,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:3,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:2,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:712345678912345,to:"Mac",rider:12345678912345,amount:1000,status:3,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:112345678912345,to:"Mac",rider:12345678912345,amount:500,status:3,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:3000,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:2,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:3,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:712345678912345,to:"Mac",rider:12345678912345,amount:900,status:3,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:112345678912345,to:"Mac",rider:12345678912345,amount:500,status:2,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:300,status:2,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:712345678912345,to:"Mac",rider:12345678912345,amount:900,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:112345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:312345678912345,to:"Marsol",rider:12345678912345 ,amount:300,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:412345678912345,to:"Mac",rider:12345678912345,amount:200,status:2,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:512345678912345,to:"Marsol",rider:12345678912345,amount:400,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:612345678912345,to:"Mac",rider:12345678912345,amount:500,status:1,sender:12345678912345,from:"EgyptPost",type:"Pos"},
+    {id:712345678912345,to:"Mac",rider:12345678912345,amount:900,status:2,sender:12345678912345,from:"EgyptPost",type:"Pos"},
   ];
   listofcompanies:Array<any>=[]
   random:string="";
@@ -70,7 +70,20 @@ export class PaymentaddComponent implements OnInit {
     this.checkontransId()
     
   }
-
+  selected:any;
+  calculateselect(e:any){
+    console.log("================")
+    this.selected=0;
+    e.classList.toggle("select");
+    var all=document.getElementsByClassName("checkrow");
+    for (let index = 0; index < all.length; index++) {
+      const element = all[index];
+      if (element.classList.contains("select")) {
+        this.selected++;
+      }
+      console.log(this.selected)
+    }
+  }
     checkontransId(){
       for (let index = 0; index < 2; index++) {
         let id=this.uuid();
